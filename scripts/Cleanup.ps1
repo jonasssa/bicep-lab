a479f7fa33b778aff3a0680b9df0bc40471b2888
Jonas Save <jonas.save@devoteam.no>
2022-12-12 16:33:19 +0100

    Added cleanup step

---
+++ scripts/Cleanup.ps1
@@ -0,0 +1,33 @@
+param (
+    [Parameter()]
+    [boolean]
+    $RemoveLocks = $true,
+
+    [Parameter()]
+    [string[]]
+    $excludedResourceGroupNames = @(
+        'AzureBackupRG_westeurope_1'
+        'AzureBackupRG_westeurope_2'
+        'AzureBackupRG_westeurope_3'
+        'AzureBackupRG_westeurope_4'
+        'AzureBackupRG_westeurope_5'
+        'lz'
+        'lz-network'
+        'NetworkWatcherRG'
+    )
+)
+#* Install modules
+scripts/helpers/Install-Modules.ps1 -Modules @{
+    "Az.Resources" = "latest"
+}
+
+#* Get resourcegroups that are not part of the excluded tag nor groups containing the keep tag
+$resources = Get-AzResource -ResourceGroupName "jonas-test-rg"
+
+#* Removing all resources
+$resources | ForEach-Object -parallel {
+    $ResourceName = $_.Name
+    Write-Host "Removing resources in $ResourceName" 
+
+    Remove-AzResource -Name $ResourceName -ResourceGroupName "jonas-test-rg" -Force -Confirm:$false
+}
---
+++ scripts/helpers/LoginAzServicePrincipal.ps1
@@ -0,0 +1,25 @@
+################### WARNING #########################
+## This file is managed by workload-repo-template  ##
+## Local changes will be overwritten               ##
+#####################################################
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ServicePrincipalJson = $env:CREDENTIALS
+)
+$ProgressPreference = "SilentlyContinue"
+
+## Install required modules
+scripts/helpers/Install-Modules.ps1 -Modules @{
+    "Az.Accounts" = "latest"
+}
+
+## Login
+Write-Host "Start login with SPN"
+$cred = $servicePrincipalJson | ConvertFrom-Json
+
+$securePass = ConvertTo-SecureString -String $cred.clientSecret -AsPlainText -Force
+$credential = [System.Management.Automation.PSCredential]::new($cred.appId, $securePass)
+Connect-AzAccount -Credential $credential -ServicePrincipal -TenantId $cred.tenantId -Subscription $cred.subscriptionId -WarningAction Ignore | Out-Null
