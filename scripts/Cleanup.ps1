c64e7dc6b7b36094286e16f5352d679ec252d78b
Jonas Save <jonas.save@devoteam.no>
2022-12-14 09:09:18 +0100

    Fix typo

--- scripts/Cleanup.ps1
+++ scripts/Cleanup.ps1
@@ -6,7 +6,7 @@ scripts/helpers/Install-Modules.ps1 -Modules @{
 #* Get resourcegroups that are not part of the excluded tag nor groups containing the keep tag
 $ResourceGroups = Get-AzResourceGroup
 foreach($ResourceGroup in $ResourceGroups){
-    $resources = Get-AzResource -ResourceGroupName $ResourceGroup.Name
+    $resources = Get-AzResource -ResourceGroupName $ResourceGroup.ResourceGroupName
 
     #* Removing all resources
     $resources | ForEach-Object -parallel {
